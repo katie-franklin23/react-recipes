3e3442484ed2b8fdbc90b0ecbafd860cda089a02
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-18 13:11:59 +1300

    react icons installed

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const TopNav = () => {
-  return <div>TopNav</div>
+  return (
+    <div className="max-w-[1520] mx-auto flex justify-between items-center p-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer"></div>
+      </div>
+      TopNav
+    </div>
+  )
 }
 
 export default TopNav
