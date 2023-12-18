8727f78297cbabf990bac6ab9566a3922e941d1a
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-18 16:49:19 +1300

    console logs are working for setSideNav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   AiOutlineMenu,
   AiOutlineSearch,
@@ -8,10 +8,12 @@ import {
 import { BsFillCartFill, BsPerson } from 'react-icons/bs'
 
 const TopNav = () => {
+  const [sideNav, setSideNav] = useState(false)
+  console.log(sideNav)
   return (
     <div className="max-w-[1520] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setSideNav(!sideNav)} className="cursor-pointer">
           <AiOutlineMenu size={25} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
