577ea186913009c0634834eb866dc7d2d5931cfa
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-18 13:45:06 +1300

    styling applied to free delivery

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -17,6 +17,12 @@ const TopNav = () => {
           Yum
           <span>Eats</span>
         </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-orange-700 text-white rounded full p-2 text-bold">
+            Free
+          </p>
+          <p className="p-2 text-bold">Delivery</p>
+        </div>
       </div>
     </div>
   )
