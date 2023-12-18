01ccc0454d2c17352c8a29bab7d47488f64ee433
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-18 15:59:17 +1300

    removed outline colour from search bar

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -26,6 +26,7 @@ const TopNav = () => {
       </div>
       <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
         <AiOutlineSearch size={25} />
+        <input className="bg-transparent p-2 w-full focus:outline-none"></input>
       </div>
     </div>
   )
