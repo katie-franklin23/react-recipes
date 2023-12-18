c0cc9330898fa0ed9d595c7b228ecf24760af516
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-18 13:47:28 +1300

    updated round border on free

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -18,7 +18,7 @@ const TopNav = () => {
           <span>Eats</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
-          <p className="bg-orange-700 text-white rounded full p-2 text-bold">
+          <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
             Free
           </p>
           <p className="p-2 text-bold">Delivery</p>
