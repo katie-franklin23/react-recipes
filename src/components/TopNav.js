eff0fde790a6c143183c059ce32e432d767f952a
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-19 18:55:33 +1300

    move exit icon for sidebar to right hand side

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -50,7 +50,11 @@ const TopNav = () => {
             : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'
         }
       >
-        <AiOutlineClose onClick={() => setSideNav(!sideNav)} size={25} />
+        <AiOutlineClose
+          onClick={() => setSideNav(!sideNav)}
+          size={25}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
       </div>
     </div>
   )
