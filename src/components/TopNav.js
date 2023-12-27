b96b185b2fe702d0b4f0e455df6bf399f9c21571
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-27 16:05:46 +1300

    icon person displays

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -61,6 +61,10 @@ const TopNav = () => {
         <nav>
           <ul className="flex flex-col p-4 text-gray-900">
             <li className="text-xl py-4 flex"></li>
+            <BsPerson
+              size={25}
+              className="mr-4 text-white bg-black rounded-full"
+            />
           </ul>
         </nav>
       </div>
