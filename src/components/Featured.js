d648923ce2bb47a883cd252e901258f38528fd38
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-12 10:37:57 +1300

    apply style to background image

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -13,7 +13,12 @@ const Featured = () => {
     },
   ]
   const [currentIndex, setCurrentIndex] = useState(0)
-  return <div>Featured</div>
+  return (
+    <div className="max-w-[1520px] h-[500px] w-full py-4 px-4 relative">
+      <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
+      style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}
+    </div>
+  )
 }
 
 export default Featured
