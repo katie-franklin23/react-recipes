5ef753b4eba6d2b20ab28a90f54fdb441f63c05b
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-12 17:28:45 +1300

    adding functionality to left click arrow

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -13,7 +13,13 @@ const Featured = () => {
       url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672612/NetflixApp/ric_a4ewxo.jpg',
     },
   ]
-  const [currentIndex, setCurrentIndex] = useState(2)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const prevSiider = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+  }
   return (
     <div className="max-w-[1520px] h-[500px] w-full py-4 px-4 relative group">
       <div
@@ -21,7 +27,7 @@ const Featured = () => {
         style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}
       ></div>
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer ">
-        <BsChevronCompactLeft />
+        <BsChevronCompactLeft onClick={prevSiider} />
       </div>
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer ">
         <BsChevronCompactRight />
