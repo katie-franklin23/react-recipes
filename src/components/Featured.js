b0e413bab6396f4ccadc5bb1bc483daaae34f4e0
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-12 17:16:54 +1300

    fix alignment so arrow shows on left hand side

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -13,14 +13,14 @@ const Featured = () => {
       url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672612/NetflixApp/ric_a4ewxo.jpg',
     },
   ]
-  const [currentIndex, setCurrentIndex] = useState(1)
+  const [currentIndex, setCurrentIndex] = useState(2)
   return (
     <div className="max-w-[1520px] h-[500px] w-full py-4 px-4 relative">
       <div
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
         style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}
       ></div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer ">
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer ">
         <BsChevronCompactLeft />
       </div>
     </div>
