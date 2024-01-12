46f066a48e769c8beb81dd47ebd0054c7dcbde1f
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-13 10:54:15 +1300

    render Delivery in app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import TopNav from './components/TopNav'
 import Featured from './components/Featured'
+import Delivery from './components/Delivery'
 
 function App() {
   return (
     <div className="App">
       <TopNav />
       <Featured />
+      <Delivery />
     </div>
   )
 }
