6e2c18298f7b3f353dd04f2fe79a7cc948a06ec2
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-14 09:14:27 +1300

    stylize h1 text

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const TopPicks = () => {
-  return <div>TopPicks</div>
+  return (
+    <>
+      <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+        Top Picks
+      </h1>
+    </>
+  )
 }
 
 export default TopPicks
