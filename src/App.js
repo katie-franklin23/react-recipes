db9312664742e5b4703105514540ea79736e015b
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-14 09:15:35 +1300

    render top picks in app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import TopNav from './components/TopNav'
 import Featured from './components/Featured'
 import Delivery from './components/Delivery'
+import TopPicks from './components/TopPicks'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <TopNav />
       <Featured />
       <Delivery />
+      <TopPicks />
     </div>
   )
 }
