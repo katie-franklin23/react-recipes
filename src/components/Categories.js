f3fccbac8df0adf08904252124672f06d3501271
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-16 11:18:08 +1300

    change pixels

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,7 @@ import { categories } from '../data'
 const Categories = () => {
   console.log(categories)
   return (
-    <div className="max-w-[1520px] m-auto px-4 py-12">
+    <div className="max-w-[1520px] m-auto px-4 py-4">
       <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
         Trending Categories
       </h1>
