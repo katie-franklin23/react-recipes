0ca0b5ee88de5f6a9751de8fe1733a40e202be07
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-16 11:28:35 +1300

    add images

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -8,7 +8,15 @@ const Categories = () => {
       <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
         Trending Categories
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-6 gap-5 py-5 px-2"></div>
+      <div className="grid grid-cols-2 md:grid-cols-6 gap-5 py-5 px-2">
+        {categories.map((item) => {
+          return (
+            <div className="p-4 flex justify-center items-center hover:scale-105 duration-300">
+              <img src={item.image} alt={item.name} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
