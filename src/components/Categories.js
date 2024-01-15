a9efec6d2ff33e713ace6296d1e20dc617ccc44a
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-16 11:17:31 +1300

    add heading

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { categories } from '../data'
 
 const Categories = () => {
-  return <div>Categories</div>
+  console.log(categories)
+  return (
+    <div className="max-w-[1520px] m-auto px-4 py-12">
+      <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+        Trending Categories
+      </h1>
+    </div>
+  )
 }
 
 export default Categories
