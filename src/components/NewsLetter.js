29fef9d06d0c0f7d2d29d0af347d801aa3174eac
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-17 10:28:12 +1300

    fix margin

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -4,7 +4,7 @@ const NewsLetter = () => {
   return (
     <div className="max-w-[1520px] m-auto text-white px-4 bg-[#24262b]">
       <div className="mx-auto grid lg:grid-cols-3">
-        <div className="lg:col-span-2 my -4"></div>
+        <div className="lg:col-span-2 my-4"></div>
         <h1>Need advice on how to improve your flow?</h1>
         <p>Sign Up to join our newsletter and stay up to date</p>
       </div>
