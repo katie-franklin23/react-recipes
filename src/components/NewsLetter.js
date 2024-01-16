8634c939c35fb94457f9202e78bc908ab80f725c
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-17 09:40:19 +1300

    apply styling

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const NewsLetter = () => {
-  return <div>NewsLetter</div>
+  return (
+    <div className="max-w-[1520px] m-auto text-white px-4 bg-[#24262b]">
+      <div className="mx-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2 my -4"></div>
+      </div>
+    </div>
+  )
 }
 
 export default NewsLetter
