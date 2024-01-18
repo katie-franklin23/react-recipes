3b57c5dfcd191d73f0c4ec8ff6034f97548b41b6
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-19 03:20:50 +1300

    add location heading

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,14 +17,19 @@ const Footer = () => {
             Lorem, ipsum dolar sit amet conectetur apidipsicing elit. Id odit
             ullam iste repellat consequatur libero reiciendis, blanditiis
             accusantium.
-            <div className="flex justify-between md:w-[75%] my-6">
-              <FaFacebookSquare size={30} />
-              <FaInstagram size={30} />
-              <FaTwitterSquare size={30} />
-              <FaGithubSquare size={30} />
-              <FaDribbbleSquare size={30} />
-            </div>
           </p>
+          <div className="flex justify-between md:w-[75%] my-6">
+            <FaFacebookSquare size={30} />
+            <FaInstagram size={30} />
+            <FaTwitterSquare size={30} />
+            <FaGithubSquare size={30} />
+            <FaDribbbleSquare size={30} />
+          </div>
+        </div>
+        <div className="lg:col-span-2 flex justify-between mt-6">
+          <div>
+            <h6 className="font-medium text-[#9b9b9b]">Location</h6>
+          </div>
         </div>
       </div>
     </div>
