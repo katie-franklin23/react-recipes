1ef18eec868036f71715edb9435728ca0ed92bf2
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2024-01-19 02:35:53 +1300

    add styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,11 @@ import {
 } from 'react-icons/fa'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
+      <div className="py-16 px-4 grid lg:grid-cols3 gap-8 text-gray-300>"></div>
+    </div>
+  )
 }
 
 export default Footer
